4cb2035def03b9a95fd1be42e8f2baf458f38ad5
Onanii Yui <177497261+yui-onanii@users.noreply.github.com>
2024-11-04 16:23:17 +0900

    redmi9a: Add MT6765 SoC
    Signed-off-by: Onanii Yui <177497261+yui-onanii@users.noreply.github.com>

--- hw/arm/redmi9a.c
+++ hw/arm/redmi9a.c
@@ -1,13 +1,23 @@
 #include "qemu/osdep.h"
+#include "qapi/error.h"
+#include "hw/qdev-core.h"
 #include "hw/boards.h"
+#include "hw/arm/mt6765.h"
+#include "qom/object.h"
 
 static void redmi9a_init(MachineState *machine)
 {
+    DeviceState *mt6762g;
+
+    // (almost) identical SoCs with slightly slower clock
+    mt6762g = qdev_new(TYPE_MT6765);
+    object_property_add_child(OBJECT(machine), "soc", OBJECT(mt6762g));
+    qdev_realize_and_unref(DEVICE(mt6762g), NULL, &error_fatal);
 }
 
 static void redmi9a_machine_init(MachineClass *mc)
 {
-    mc->desc = "Xiaomi Redmi 9A";
+    mc->desc = "Xiaomi Redmi 9A (MT6762G)";
     mc->init = redmi9a_init;  // must have
 }
 
